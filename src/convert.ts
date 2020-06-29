e30127c39e7e25034d6300196336d406a5fead6c
Dmitry Orabey <dmitry.orabey@gmail.com>
2020-06-29 22:27:17 +0300

    update

---
+++ src/convert.ts
@@ -0,0 +1,76 @@
+import { promises as fsAsync } from "fs";
+import AWS from "aws-sdk";
+import exec from "await-exec";
+import { S3Handler } from "aws-lambda";
+import path from "path";
+
+const s3 = new AWS.S3();
+
+const saveToTmp = async (params: AWS.S3.GetObjectRequest) => {
+  const data = await s3.getObject(params).promise();
+  await fsAsync.writeFile(`/tmp/${params.Key}`, data.Body);
+};
+
+const deleteObject = async (
+  params: AWS.S3.DeleteObjectRequest,
+  folder: string
+) => {
+  console.log("delete", params);
+  await s3.deleteObject(params).promise();
+  await exec(`rm -rf ${folder}`);
+};
+
+const saveResult = async (distFolderPath: string, folder: string) => {
+  const files = await fsAsync.readdir(distFolderPath);
+  if (!files || files.length === 0) {
+    console.log(
+      `provided folder '${distFolderPath}' is empty or does not exist.`
+    );
+    console.log("Make sure your project was compiled!");
+    return;
+  }
+
+  for (const fileName of files) {
+    const filePath = path.join(distFolderPath, fileName);
+    const fileContent = await fsAsync.readFile(filePath);
+    await s3
+      .putObject({
+        Bucket: process.env.IMG_BUCKET,
+        Key: `${folder}/${fileName}`,
+        Body: fileContent,
+      })
+      .promise();
+
+    console.log(`Successfully uploaded '${fileName}'!`);
+  }
+};
+
+export const index: S3Handler = async (event) => {
+  console.log(event);
+  const bucket = event.Records[0].s3.bucket.name;
+  const srcKey = event.Records[0].s3.object.key;
+  const dstPrefix = `${srcKey.replace(/\.\w+$/, "")}`;
+  const fileType = srcKey.slice(-3, srcKey.length);
+
+  if (!fileType || fileType !== "pdf") {
+    console.log(`Invalid filetype found for key: ${srcKey}`);
+  }
+
+  console.log(bucket, srcKey, dstPrefix, fileType);
+
+  await saveToTmp({ Bucket: bucket, Key: srcKey });
+
+  const splitted = dstPrefix.split("--separator--");
+
+  await exec(
+    `mkdir -p /tmp/${
+      splitted[0]
+    } && pdftocairo -jpeg -r 200 -singlefile -cropbox -jpegopt "quality=80" "/tmp/${srcKey}" "/tmp/${splitted.join(
+      "/"
+    )}"`
+  );
+
+  await saveResult(`/tmp/${splitted[0]}`, splitted[0]);
+
+  await deleteObject({ Bucket: bucket, Key: srcKey }, `/tmp/${splitted[0]}`);
+};
---
+++ src/split.ts
@@ -0,0 +1,74 @@
+import { promises as fsAsync } from "fs";
+import AWS from "aws-sdk";
+import exec from "await-exec";
+import { S3Handler } from "aws-lambda";
+import path from "path";
+
+const s3 = new AWS.S3();
+
+const saveToTmp = async (params: AWS.S3.GetObjectRequest) => {
+  const data = await s3.getObject(params).promise();
+  await fsAsync.writeFile(`/tmp/${params.Key}`, data.Body);
+};
+
+const saveResult = async (distFolderPath: string) => {
+  const files = await fsAsync.readdir(distFolderPath);
+  if (!files || files.length === 0) {
+    console.log(
+      `provided folder '${distFolderPath}' is empty or does not exist.`
+    );
+    console.log("Make sure your project was compiled!");
+    return;
+  }
+
+  for (const fileName of files) {
+    const filePath = path.join(distFolderPath, fileName);
+    const fileContent = await fsAsync.readFile(filePath);
+    await s3
+      .putObject({
+        Bucket: process.env.PAGES_BUCKET,
+        Key: fileName,
+        Body: fileContent,
+      })
+      .promise();
+
+    console.log(`Successfully uploaded '${fileName}'!`);
+  }
+};
+
+export const index: S3Handler = async (event) => {
+  console.log(event);
+  const bucket = event.Records[0].s3.bucket.name;
+  const srcKey = event.Records[0].s3.object.key;
+  const dstPrefix = `${srcKey.replace(/\.\w+$/, "")}`;
+  const fileType = srcKey.slice(-3, srcKey.length);
+
+  if (!fileType || fileType !== "pdf") {
+    console.log(`Invalid filetype found for key: ${srcKey}`);
+  }
+
+  console.log(bucket, srcKey, dstPrefix, fileType);
+
+  await saveToTmp({ Bucket: bucket, Key: srcKey });
+
+  const result = await exec(`pdfinfo /tmp/${srcKey}`);
+
+  const totalPages = result.stdout.match(/Pages:\s*(.*)\n/)[1];
+  const batch = 50;
+  const pages = [...Array(Math.ceil(totalPages / batch)).keys()];
+
+  for (const page of pages) {
+    let l = (page + 1) * batch;
+    if (l > totalPages) l = totalPages;
+    const res = await exec(
+      `mkdir -p /tmp/${dstPrefix} && pdfseparate -f ${
+        page * batch + 1
+      } -l ${l} /tmp/${srcKey} /tmp/${dstPrefix}/${dstPrefix}--separator--%d.pdf`
+    );
+    console.log(res);
+    await saveResult(`/tmp/${dstPrefix}`);
+    console.log("save result");
+    await exec(`rm -rf /tmp/${dstPrefix}`);
+    console.log("remove");
+  }
+};
